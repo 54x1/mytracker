12895cfe6f29c6a097335555298fa9ba525a9c20
54x1 <five4x1@gmail.com>
2022-06-29 11:22:48 +0000

    update add view

--- src/views/Page-Add.vue
+++ src/views/Page-Add.vue
@@ -5,8 +5,7 @@
         <div class="col-md-12 col-lg-8 offset-md-0 offset-lg-2">
           <h1>Add Item</h1>
           <form @submit.prevent="addItem()" class="form-inline">
-            <div class="d-flex">
-            <div class="form-group">
+            <div class="form-group w-100">
               <input
                 type="text"
                 class="form-control mb-2"
@@ -14,29 +13,37 @@
                 v-model="name"
               />
             </div>
-            <div class="form-group">
-              <input
-                type="number"
-                step="any"
-                class="form-control mb-2"
-                placeholder="Enter Amount"
-                v-model="amount"
-              />
+            <div class="d-flex justify-content-center mb-2">
+              <div class="form-group w-100">
+                <input
+                  type="number"
+                  step="any"
+                  class="form-control mb-0"
+                  placeholder="Enter Amount"
+                  v-model="amount"
+                />
+              </div>
+              <select class="form-select w-50 mb-2" v-model="selected">
+                <option
+                  v-for="option in options"
+                  :key="option"
+                  :value="`${option.value}`"
+                >
+                  {{ option.period }}
+                </option>
+              </select>
             </div>
-            <select class="form-select w-50 mb-2" v-model="selected">
-  <option v-for="option in options" :key="option" :value="`${option.value}`">{{option.period}}</option>
-</select>
-              <!-- <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">{{value}}</button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" role="menu">
-      <li v-for="option in options" :key="option">
-        <a class="dropdown-item"  @click="value = option" href="javascript:void(0)">{{option}}</a>
-      </li>
-    </ul>
-  </div> -->
-  </div>
-            <button type="submit" class="w-100 btn btn-primary">Add Item</button>
-          </form>
+           
+            <span class="d-flex justify-content-center">
+              <button type="submit" class="w-50 btn btn-primary">
+              Add Item
+            </button> 
+            <a @click="pathTo()" class="w-50 btn btn-danger">
+              Cancel
+            </a>
+            </span>
+             </form>
+          
         </div>
       </div>
     </div>
@@ -44,18 +51,18 @@
 </template>
 
 <script>
-import {itemsColRef} from '../firebase'
-import {addDoc} from 'firebase/firestore'
+import { itemsColRef } from "../firebase";
+import { addDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-let userId
+let userId;
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
-     userId = user.uid;
+    userId = user.uid;
   }
 });
 export default {
-  data(){
+  data() {
     return {
       userId: userId,
       name: null,
@@ -63,24 +70,25 @@ export default {
       category: this.$route.params.catId,
       selected: "52",
       options: [
-            { period: "Weekly", value: "52" },
-            { period: "Fortnightly", value: "26" },
-            { period: "Monthly", value: "12" },
-            { period: "Quarterly", value: "4" },
-            { period: "Annually", value: "1" }
-            ],
-    }
+        { period: "Weekly", value: "52" },
+        { period: "Fortnightly", value: "26" },
+        { period: "Monthly", value: "12" },
+        { period: "Quarterly", value: "4" },
+        { period: "Annually", value: "1" },
+      ],
+    };
   },
   methods: {
+        pathTo() {
+      this.$router.push("/");
+    },
     async addItem() {
-
-      const addedDoc = await addDoc(itemsColRef, this.$data)
-            console.log('creating here', addedDoc)
-            // alert("Item Added!")
-            this.$router.push("/")
-    }
-
-  }
+      const addedDoc = await addDoc(itemsColRef, this.$data);
+      console.log("creating here", addedDoc);
+      // alert("Item Added!")
+      this.$router.push("/");
+    },
+  },
 };
 </script>
 
